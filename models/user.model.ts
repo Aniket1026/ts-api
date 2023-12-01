3e4130826646c1bea8ee28caa77330193237bb27
Aniket <annudubey1026@gmail.com>
2023-12-02 00:43:23 +0530

    feat: Implement pre-save hook in Mongo to hash passwords

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,40 +1,50 @@
 import mongoose from "mongoose";
-import {z} from "zod";
+import bcrypt from "bcryptjs";
+import { z } from "zod";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        min: [6,"Too short name"],
-        max: 32,
-        unique: true
+      type: String,
+      required: true,
+      min: [6, "Too short name"],
+      max: 32,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 32
+      type: String,
+      required: true,
+      min: 6,
+      max: 32,
     },
     password: {
-        type: String,
-        required: true,
-        min: [6,"Too short password"],
-        max: 32
+      type: String,
+      required: true,
+      min: [6, "Too short password"],
+      max: 32,
     },
     role: {
-        type: String,
-        enum: ["user","admin"],
-        default: "user"
-    }
-}, { timestamps: true });
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
 
 export const userSchemaZod = z.object({
-    username: z.string().min(6).max(32),
-    email: z.string().email(),
-    password: z.string().min(6).max(32),
-    role: z.enum(["user","admin"])
-})
+  username: z.string().min(6).max(32),
+  email: z.string().email(),
+  password: z.string().min(6).max(32),
+  role: z.enum(["user", "admin"]),
+});
 
-export type UserSchema = z.infer<typeof userSchemaZod>
-export const User = mongoose.model("User", userSchema)
+export type UserSchema = z.infer<typeof userSchemaZod>;
+export const User = mongoose.model("User", userSchema);
